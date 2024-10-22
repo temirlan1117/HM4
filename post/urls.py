1ecbf08ed8c554df185ef51dcc11626f748c04bd
temirlan1117 <temirlanormushev@yandex.ru>
2024-10-22 14:04:43 +0300

    HM1

---
+++ post/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+urlpatterns = [
+    path('http_response', views.http_response, ),
+    path('html_response', views.html_response, ),
+
+
+
+
+]
